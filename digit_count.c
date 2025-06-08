1d9a4012107c91c41ffa5119ec1f56200bff3dd1
Tubhyam14 <tubhyamjain14@gmail.com>
2025-06-08 16:20:20 +0530

    corrected the wrong code in digit_count.c

--- digit_count.c
+++ digit_count.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    int digit = 0;
-    int flag = 1;
+    int digit;
     int count = 0;
+
     printf("Enter a digit\n");
-    scanf(" %d", &digit);
+    scanf("%d", &digit);
+
     int temp = digit;
 
-    while (flag)
-    {
-        digit = digit / 10;
-        count++;
-        if (digit == 0)
-        {
-            count = 1;
+    if (digit == 0) {
+        count = 1;
+    } else {
+        while (digit != 0) {
+            digit = digit / 10;
+            count++;
         }
-        if (digit == 0)
-            break;
     }
 
     printf("%d has %d digits\n", temp, count);
