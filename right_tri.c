e6256a9861792725502e5d039227a5cb0102da46
Tubhyam14 <tubhyamjain14@gmail.com>
2025-06-17 23:58:44 +0530

    right trianglr pattern

---
+++ right_tri.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+
+    for(int i=1;i<=5;i++){
+        for(int space=1;space <=5-i;space++)printf(" ");
+        for(int j=0;j<i;j++){
+            printf("%c",'*');
+        }
+      printf("\n");
+
+    }
+
+return 0;
+}
